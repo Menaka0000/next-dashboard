3d30c22020d48e072a72a206ac171b9aa1ff1363
Menaka0000 <m.lakshan0000@gmail.com>
2024-02-08 06:53:03 +0530

    added missing content

---
+++ src/components/main-content-card.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainContentCard = () => {
+    return (
+        <div className='w-full min-h-[100px] bg-slate-50 shadow-md rounded p-2 '>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam at cum dicta dolor eius, eveniet,</p>
+        </div>
+    );
+};
+
+export default MainContentCard;
